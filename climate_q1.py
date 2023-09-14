d0fd69e421d1f88ccdd80af3138b3f947bf80bf8
wsq94317 <wsq94317@vip.qq.com>
2023-09-14 21:30:16 +1000

    submit climate_q1.py, add function to load data from climate.db

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+database = sqlite3.connect("./climate.db")
+cursor = database.cursor()
+cursor.execute("SELECT * FROM ClimateData")
+results = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
+
+for row in results:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+database.close()
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
